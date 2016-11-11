5d05416473b91f388ffe553ec088acd743ce1da5
Rico Suter <mail@rsuter.com>
2016-11-11 11:54:13 +0100

    Fix

--- src/VisualJsonEditor/Converters/NumberConverter.cs
+++ src/VisualJsonEditor/Converters/NumberConverter.cs
@@ -16,7 +16,7 @@ namespace VisualJsonEditor.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return value?.ToString();
+            return value != null ? value.ToString() : null;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
